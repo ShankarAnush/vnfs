4d0b04a7539e030ae181e3e2f99047043b5a73dc
ShankarAnush <anushalva3@gmail.com>
2019-08-25 01:42:20 +0200

    changes to access the log files

--- docker/haproxy_client/script.sh
+++ docker/haproxy_client/script.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-curl -x 172.17.0.4:80 ab -n 10000 -c 30 http://172.17.0.3/ > test.txt
+curl -L http://172.18.0.4:80/ &&
+ab -n 10000 -c 30 http://172.18.0.4:80/ > test.txt
 
 exec "$@"
